f1b728900fb01a2a7302907ea9f882d9b668f944
artem <artem.yurev.1997@gmail.com>
2023-04-09 16:03:21 +0300

    task2

---
+++ src/by/pvt/lessonten/Robots.java
@@ -0,0 +1,72 @@
+package by.pvt.lessonten;
+
+/*public abstract class Robots implements Robot {
+    private String model;
+    private int power;
+    private String powerUnit;
+    private String country;
+
+    public Robots(String model, int power, String powerUnit, String country) {
+        this.model = model;
+        this.power = power;
+        this.powerUnit = powerUnit;
+        this.country = country;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getPowerUnit() {
+        return powerUnit;
+    }
+
+    public void setPowerUnit(String powerUnit) {
+        this.powerUnit = powerUnit;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public abstract void uniquePossibility();
+
+
+     private Robots[] robots = new Robots[0];
+
+    public void getRobotsExhibitions(Robots robot) {
+        int numOfLength = robots.length;
+        if(numOfLength == 0) {
+            robots = new Robots[1];
+            robots[0] = robot;
+        }
+        else {
+            Robots[] copyRobots = new Robots[robots.length];
+            for(int i =0; i < robots.length; i++) {
+                if(i != robots.length) {
+                    copyRobots[i] = robots[i];
+                }
+                else {
+                    copyRobots[i] = robot;
+                }
+            }
+            robots = copyRobots;
+        }
+    }
+}*/
